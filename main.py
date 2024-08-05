6bcc543ba69c239cfafc1e4c24a3d0b775b7a8cf
3xtraaa <164105566+3xtraaa@users.noreply.github.com>
2024-08-05 01:34:36 +0000

    Main py flask file, coinnecting db

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error
 
 app = Flask(__name__)
+DATABASE = "nancy_drew_ctlg.db"
 
+def create_connection(db_file):
+    """
+    Creates a connection to the database
+    :parameter    db_file - the name of the file
+    :returns    connection - a connection to the database
+    """
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+    return None
 
 @app.route('/')
 def render_home():
